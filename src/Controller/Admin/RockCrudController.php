d69624e24792e1975343dec1fa0836e3cc795acb
Bernhard Ullmann <ullmann@neubaukompass.de>
2025-10-20 15:57:12 +0200

    revert

--- src/Controller/Admin/RockCrudController.php
+++ src/Controller/Admin/RockCrudController.php
@@ -3,11 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Rock;
-use App\Entity\RockTranslation;
 use App\Form\Type\JsonFieldType;
-use App\Repository\RockTranslationRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
@@ -25,13 +21,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class RockCrudController extends AbstractCrudController
 {
-    public function __construct(
-        private readonly RockTranslationRepository $rockTranslationRepository,
-        private readonly EntityManagerInterface $entityManager,
-        private readonly RequestStack $requestStack,
-    ) {
-    }
-
     public static function getEntityFqcn(): string
     {
         return Rock::class;
@@ -108,30 +97,6 @@ class RockCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        $currentRequest = $this->requestStack->getCurrentRequest();
-        $adminContext = $currentRequest?->attributes->get('ea');
-        $currentEntity = $adminContext?->getEntity()?->getInstance();
-
-        $descriptionDe = null;
-        $descriptionEn = null;
-        $natureDe = null;
-        $natureEn = null;
-        $accessDe = null;
-        $accessEn = null;
-        $flowersDe = null;
-        $flowersEn = null;
-
-        if ($currentEntity instanceof Rock) {
-            $descriptionDe = $this->getTranslationFieldValue($currentEntity, 'de', 'description');
-            $descriptionEn = $this->getTranslationFieldValue($currentEntity, 'en', 'description');
-            $natureDe = $this->getTranslationFieldValue($currentEntity, 'de', 'nature');
-            $natureEn = $this->getTranslationFieldValue($currentEntity, 'en', 'nature');
-            $accessDe = $this->getTranslationFieldValue($currentEntity, 'de', 'access');
-            $accessEn = $this->getTranslationFieldValue($currentEntity, 'en', 'access');
-            $flowersDe = $this->getTranslationFieldValue($currentEntity, 'de', 'flowers');
-            $flowersEn = $this->getTranslationFieldValue($currentEntity, 'en', 'flowers');
-        }
-
         yield Field::new('name')
             ->setLabel('Name Fels')
             ->hideOnDetail()
@@ -172,21 +137,6 @@ class RockCrudController extends AbstractCrudController
             ->setHelp('Beschreibung zum Fels.')
             ->setColumns('col-12');
 
-        // Translated fields (DE/EN) - saved into RockTranslation
-        yield TextareaField::new('description_de')
-            ->setLabel('Beschreibung (DE)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $descriptionDe)
-            ->setColumns('col-12');
-
-        yield TextareaField::new('description_en')
-            ->setLabel('Beschreibung (EN)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $descriptionEn)
-            ->setColumns('col-12');
-
         yield TextareaField::new('nature')
             ->setLabel('Naturschutz')
             ->hideOnIndex()
@@ -194,55 +144,12 @@ class RockCrudController extends AbstractCrudController
             ->setColumns('col-12')
             ->setHelp('Angaben die für den Naturschutz wichtig sind.');
 
-        yield TextareaField::new('nature_de')
-            ->setLabel('Naturschutz (DE)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $natureDe)
-            ->setColumns('col-12');
-
-        yield TextareaField::new('nature_en')
-            ->setLabel('Naturschutz (EN)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $natureEn)
-            ->setColumns('col-12');
-
         yield TextareaField::new('access')
             ->setLabel('Zustieg')
             ->hideOnIndex()
             ->hideOnDetail()
             ->setColumns('col-12')
             ->setHelp('Beschreibung des Zustiegs zum Fels.');
-
-        yield TextareaField::new('access_de')
-            ->setLabel('Zustieg (DE)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $accessDe)
-            ->setColumns('col-12');
-
-        yield TextareaField::new('access_en')
-            ->setLabel('Zustieg (EN)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $accessEn)
-            ->setColumns('col-12');
-
-        // Flowers (translated)
-        yield TextareaField::new('flowers_de')
-            ->setLabel('Flora & Fauna (DE)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $flowersDe)
-            ->setColumns('col-12');
-
-        yield TextareaField::new('flowers_en')
-            ->setLabel('Flora & Fauna (EN)')
-            ->onlyOnForms()
-            ->setFormTypeOption('mapped', false)
-            ->setFormTypeOption('data', $flowersEn)
-            ->setColumns('col-12');
 
         yield BooleanField::new('train')
             ->setLabel('Anfahrt mit Zug möglich')
@@ -380,105 +287,5 @@ class RockCrudController extends AbstractCrudController
             ->hideOnIndex()
             ->hideOnDetail()
             ->setColumns('col-12');
-    }
-
-
-    private function getTranslationFieldValue(Rock $rock, string $locale, string $field): ?string
-    {
-        $translation = $this->rockTranslationRepository->findOneBy([
-            'rock' => $rock,
-            'locale' => $locale,
-        ]);
-
-        if (!$translation instanceof RockTranslation) {
-            return null;
-        }
-
-        return match ($field) {
-            'description' => $translation->getDescription(),
-            'nature' => $translation->getNature(),
-            'access' => $translation->getAccess(),
-            'flowers' => $translation->getFlowers(),
-            default => null,
-        };
-    }
-
-    private function upsertTranslation(Rock $rock, string $locale, array $data): void
-    {
-        $translation = $this->rockTranslationRepository->findOneBy([
-            'rock' => $rock,
-            'locale' => $locale,
-        ]) ?? (new RockTranslation())->setRock($rock)->setLocale($locale);
-
-        if (array_key_exists('description', $data)) {
-            $translation->setDescription((string) $data['description']);
-        }
-        if (array_key_exists('nature', $data)) {
-            $translation->setNature($data['nature'] !== null ? (string) $data['nature'] : null);
-        }
-        if (array_key_exists('access', $data)) {
-            $translation->setAccess($data['access'] !== null ? (string) $data['access'] : null);
-        }
-        if (array_key_exists('flowers', $data)) {
-            $translation->setFlowers($data['flowers'] !== null ? (string) $data['flowers'] : null);
-        }
-
-        $this->entityManager->persist($translation);
-    }
-
-    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        parent::persistEntity($entityManager, $entityInstance);
-
-        if (!$entityInstance instanceof Rock) {
-            return;
-        }
-
-        $request = $this->requestStack->getCurrentRequest();
-        $formData = $request?->request->all()['Rock'] ?? [];
-
-        $this->upsertTranslation($entityInstance, 'de', [
-            'description' => $formData['description_de'] ?? null,
-            'nature' => $formData['nature_de'] ?? null,
-            'access' => $formData['access_de'] ?? null,
-            'flowers' => $formData['flowers_de'] ?? null,
-        ]);
-
-        $this->upsertTranslation($entityInstance, 'en', [
-            'description' => $formData['description_en'] ?? null,
-            'nature' => $formData['nature_en'] ?? null,
-            'access' => $formData['access_en'] ?? null,
-            'flowers' => $formData['flowers_en'] ?? null,
-        ]);
-
-        $this->entityManager->flush();
-    }
-
-    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        parent::updateEntity($entityManager, $entityInstance);
-
-        if (!$entityInstance instanceof Rock) {
-            return;
-        }
-
-        $request = $this->requestStack->getCurrentRequest();
-        $formData = $request?->request->all()['Rock'] ?? [];
-
-        $this->upsertTranslation($entityInstance, 'de', [
-            'description' => $formData['description_de'] ?? null,
-            'nature' => $formData['nature_de'] ?? null,
-            'access' => $formData['access_de'] ?? null,
-            'flowers' => $formData['flowers_de'] ?? null,
-        ]);
-
-        $this->upsertTranslation($entityInstance, 'en', [
-            'description' => $formData['description_en'] ?? null,
-            'nature' => $formData['nature_en'] ?? null,
-            'access' => $formData['access_en'] ?? null,
-            'flowers' => $formData['flowers_en'] ?? null,
-        ]);
-
-        $this->entityManager->flush();
     }
 }

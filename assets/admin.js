5911385517bae3ba2f7ec916303e3707e35e9c8b
Bernhard Ullmann <ullmann@neubaukompass.de>
2026-01-23 12:00:00 +0100

    added map for backend

--- assets/admin.js
+++ assets/admin.js
@@ -1,2 +1,6 @@
 import './styles/admin.css';
 import './bootstrap';
+
+// Register admin-specific Stimulus controllers
+import AdminDrawMapController from './controllers/admin_draw_map_controller';
+window.Stimulus.register('admin-draw-map', AdminDrawMapController);

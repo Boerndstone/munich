caabe23b056d4bd9068c22c971dfb87b99fa0ac7
Bernhard Ullmann <ullmann@neubaukompass.de>
2026-04-23 10:13:40 +0200

    map

--- assets/controllers/map_controller.js
+++ assets/controllers/map_controller.js
@@ -1,40 +1,7 @@
 import { Controller } from "stimulus";
 import L from "leaflet";
 import "leaflet.markercluster";
-
-const ROCK_CLUSTER_MAX_RADIUS = 48;
-const ROCK_DISABLE_CLUSTERING_AT_ZOOM = 15;
-
-const PIN_SVG =
-  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 40" width="28" height="40" aria-hidden="true">' +
-  '<path fill="#ef4444" stroke="#b91c1c" stroke-width="0.75" d="M14 .5C6.5.5.5 6.4.5 13.9c0 5.2 2.6 10.4 6.5 15.8 3.2 4.4 7 8.3 7 8.3s3.8-3.9 7-8.3c3.9-5.4 6.5-10.6 6.5-15.8C27.5 6.4 21.5.5 14 .5z"/>' +
-  '<circle cx="14" cy="14" r="4.25" fill="#fff"/>' +
-  "</svg>";
-
-function createMapPinIcon() {
-  return L.divIcon({
-    html: PIN_SVG,
-    className: "main-map-pin-icon",
-    iconSize: [28, 40],
-    iconAnchor: [14, 40],
-    popupAnchor: [0, -34],
-  });
-}
-
-function bubbleIconForCount(count) {
-  const n = Math.max(0, Math.min(9999, Math.round(Number(count) || 0)));
-  const d = n < 10 ? 52 : n < 100 ? 60 : n < 1000 ? 64 : 70;
-  return L.divIcon({
-    html: `<span class="main-map-cluster-count" style="--s:${d}px">${n}</span>`,
-    className: "main-map-cluster-icon",
-    iconSize: L.point(d, d),
-    iconAnchor: L.point(d / 2, d / 2),
-  });
-}
-
-function createRockGeoClusterIcon(cluster) {
-  return bubbleIconForCount(cluster.getChildCount());
-}
+import { createMapPinIcon, rockMarkerClusterGroupOptions } from "../map/icons.js";
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
@@ -102,13 +69,7 @@ export default class extends Controller {
     }
 
     const pinIcon = createMapPinIcon();
-    this.rocksClusterGroup = L.markerClusterGroup({
-      maxClusterRadius: ROCK_CLUSTER_MAX_RADIUS,
-      spiderfyOnMaxZoom: true,
-      showCoverageOnHover: false,
-      disableClusteringAtZoom: ROCK_DISABLE_CLUSTERING_AT_ZOOM,
-      iconCreateFunction: createRockGeoClusterIcon,
-    });
+    this.rocksClusterGroup = L.markerClusterGroup(rockMarkerClusterGroupOptions());
 
     this.areaMarkers = [];
     this.areaMarkerData = [];

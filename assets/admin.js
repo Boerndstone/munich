16f10c9bfaf6867a5f8275d515e77585c6ff9afe
Bernhard Ullmann <ullmann@neubaukompass.de>
2026-05-11 12:48:58 +0200

    soratble routes

--- assets/admin.js
+++ assets/admin.js
@@ -7,6 +7,8 @@ import './bootstrap';
 // Register admin-specific Stimulus controllers
 import AdminDrawMapController from './controllers/admin_draw_map_controller';
 import AdminRouteTopoSyncController from './controllers/admin_route_topo_sync_controller';
+import RoutesSortableController from './controllers/routes-sortable_controller';
 
 window.Stimulus.register('admin-draw-map', AdminDrawMapController);
 window.Stimulus.register('admin-route-topo-sync', AdminRouteTopoSyncController);
+window.Stimulus.register('routes-sortable', RoutesSortableController);
